b8dc79599a4d06dbef4f0803d503736a1b345a7b
alex2064 <kimkimgodd@naver.com>
2023-05-05 01:20:49 +0900

    DTO 수정 Valid 추가

--- src/main/kotlin/com/example/openapi/blog/controller/BlogController.kt
+++ src/main/kotlin/com/example/openapi/blog/controller/BlogController.kt
@@ -3,6 +3,7 @@ package com.example.openapi.blog.controller
 import com.example.openapi.blog.dto.BlogDto
 import com.example.openapi.blog.entity.Word
 import com.example.openapi.blog.service.BlogService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +16,7 @@ class BlogController(
 ) {
 
     @GetMapping("")
-    fun search(@RequestBody blogDto: BlogDto): String? {
+    fun search(@RequestBody @Valid blogDto: BlogDto): String? {
         val result: String? = blogService.searchKakao(blogDto)
         return result
     }
--- src/main/kotlin/com/example/openapi/core/exception/CustomExceptionHandler.kt
+++ src/main/kotlin/com/example/openapi/core/exception/CustomExceptionHandler.kt
@@ -3,16 +3,28 @@ package com.example.openapi.core.exception
 import com.example.openapi.core.response.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-
-import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@ControllerAdvice
+@RestControllerAdvice
 class CustomExceptionHandler {
 
     @ExceptionHandler(InvalidInputException::class)
-    protected fun invalidInputException(e: InvalidInputException) =
-        ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(ErrorResponse(e.message ?: "Not Exception Message"))
+    protected fun invalidInputException(ex: InvalidInputException): ResponseEntity<ErrorResponse> {
+        val errors = ErrorResponse(ex.message ?: "Not Exception Message")
+        return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    protected fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, String>> {
+        val errors = mutableMapOf<String, String>()
+        ex.bindingResult.allErrors.forEach { error ->
+            val fieldName = (error as FieldError).field
+            val errorMessage = error.getDefaultMessage()
+            errors[fieldName] = errorMessage ?: "NULL"
+        }
+        return ResponseEntity(errors, HttpStatus.BAD_REQUEST)
+    }
 }
